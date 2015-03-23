8d67a0c4ac97b8bc545897e611dac99aa84ec9fd
mfutech <mfutech@users.noreply.github.com>
2015-03-24 00:21:17 +0100

    frameword setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: table;
+				font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				margin-bottom: 40px;
+			}
+
+			.quote {
+				font-size: 24px;
+			}
+		</style>
+
+@section('content')
+<div class="container">
+			<div class="content">
+				<div class="title">Laravel 5</div>
+				<div class="quote">{{ Inspiring::quote() }}</div>
+			</div>
+</div>
+@endsection

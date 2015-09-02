369b0c059387408f67da19aa579b201c02889253
mfutech <mfutech@users.noreply.github.com>
2015-09-03 00:09:50 +0200

    update menu for cattery in nav

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,8 @@
 					<li><a href="{{ url('/') }}">Home</a></li>
 					@if(!Auth::guest())
 						<li><a href="{{ url('/expositions') }}">Expositions</a>
-					@endif
+						<li><a href="{{ url('/catteries') }}">Chatterie</a>
+						@endif
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">

0ee9089eaade2197b6a5c7430cdff33fe0c6a036
mfutech <mfutech@users.noreply.github.com>
2017-07-02 11:31:23 +0200

    update registrer translatiokn

---
+++ resources/lang/en/register.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'ouch' => 'Ouch !',
+  'problem' => 'Your entry is not valid.',
+);
--- resources/lang/fr/register.php
+++ resources/lang/fr/register.php
@@ -1,8 +1,6 @@
 <?php
 
 return array (
-    'ouch' => 'Aïe Aïe Aïe!',
-    'problem' => 'Il y a un problème avec votre saisie.',
-    
-
+  'ouch' => 'Aïe Aïe Aïe!',
+  'problem' => 'Il y a un problème avec votre saisie.',
 );
